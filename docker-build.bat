0930a85b4acbeb754dfc1a0fec47f8fa9e1f2b3a
Mateusz Maciejewski <m.maciejewski93@gmail.com>
2022-07-22 21:10:53 +0200

    added docker

---
+++ docker-build.bat
@@ -0,0 +1,5 @@
+call mvn clean package
+docker build . -t shortener-link-generator
+docker stop shortener-link-generator
+docker rm shortener-link-generator
+docker run -d -p 8080:8080 --name=shortener-link-generator shortener-link-generator
